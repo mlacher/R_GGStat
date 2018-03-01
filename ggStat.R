69422ab723d8dbbb0a92974ac5a7f4b9ed89d636
mlacher <mlacher@live.de>
2018-03-01 10:56:09 +0100

    optical changes

--- ggStat.R
+++ ggStat.R
@@ -8,14 +8,22 @@ PilotBuild <- read_excel("C:/Users/maximilian.lacher/Documents/GitHub/R_Shares/R
                                                                "numeric", "numeric", "numeric"))
 
 
+"darkturquoise"
+
 describe(PilotBuild$Damping)
 #skew pos = distribution skewed to left side, neg = distribution skewed to right side
 # kurtosis pos = distribution thin and steep, neg = wide and flat
 
 histo<-ggplot(PilotBuild, aes(x=Damping))+
-  geom_histogram(aes (y=..density..),colour="black", fill="white")+
-                   geom_density() +
-  theme_bw()
+  geom_histogram(aes (y=..density..),colour="white", fill= "deepskyblue3")+
+  geom_density() +
+  theme_bw()+
+  theme(panel.background = element_rect(fill = NA),
+    panel.grid = element_blank(),
+    panel.grid.major.y = element_line(colour = "White"),
+    panel.grid.minor.y = element_line(colour = "White"),
+    panel.ontop = TRUE)+scale_color_brewer(palette = "Set2")
+
 
 histo
 
